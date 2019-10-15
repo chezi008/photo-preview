dbb88869c52d00cff0e2baf68e106caed5b4ce21
sailor <552104693@qq.com>
2019-10-15 11:55:27 +0800

    init

--- app/src/main/java/com/example/photopreview/MainActivity.java
+++ app/src/main/java/com/example/photopreview/MainActivity.java
@@ -24,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         findViewById(R.id.btnPreview).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
